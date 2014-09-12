7cb9bd623cf55009d0d4e3e15973160f043b2d65
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2014-09-12 13:01:26 +0200

    Write 'running', delete bad files and more

--- handin_code/eval.sh
+++ handin_code/eval.sh
@@ -5,8 +5,8 @@ echo "Question 2:"
 python3 exc2test.py exc2.cost eval.fasta seq1 seq2 --backtrack --affine
 echo
 echo "Question 3:" 
-python3 exc2test.py exc2.cost eval.fasta seq1 seq2 --backtrack --matrix
+python3 exc2test.py exc2.cost eval.fasta seq1 seq2 --matrix
 echo
 echo "Question 4:" 
-python3 exc2test.py exc2.cost eval.fasta seq1 seq2 --backtrack --affine --matrix
+python3 exc2test.py exc2.cost eval.fasta seq1 seq2 --affine --matrix
 
