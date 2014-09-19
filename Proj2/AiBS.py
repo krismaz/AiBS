90c0af296ec347e9a89d76a31dc2dddfe627bd6a
Frederik Hansen <frederiklhansen@gmail.com>
2014-09-19 14:07:41 +0200

    ...

--- Proj2/AiBS.py
+++ Proj2/AiBS.py
@@ -15,7 +15,7 @@ def complex_y(a, b):
 undef = float("inf")
 
 
-def global_linear(seq1, seq2, y, gapcost, bactrack = False):
+def global_linear(seq1, seq2, y, gapcost, backtrack = False, matrix = False):
     seq1 = seq1.lower()
     seq2 = seq2.lower()
     D = numpy.zeros(shape=(len(seq1)+1,len(seq2)+1))
@@ -35,8 +35,8 @@ def global_linear(seq1, seq2, y, gapcost, bactrack = False):
             v3 = D[i-1, j] + gapcost
             D[i, j] = min(v1, v2, v3)
 
-    if not bactrack:
-        return D
+    if not backtrack:
+        return D if matrix else D[len(seq1), len(seq2)]
 
     upperAlign = ''
     lowerAlign = ''
@@ -58,16 +58,13 @@ def global_linear(seq1, seq2, y, gapcost, bactrack = False):
             lowerAlign = lowerAlign + '-'
             i = i-1
 
-    print(upperAlign[::-1])
-    print(lowerAlign[::-1])
+    return (upperAlign[::-1], lowerAlign[::-1])
 
-    return D[len(seq1), len(seq2)]
-
-def exact_3(seq1, seq2, seq3, y, gapcost, backtrack = False): 
+def exact_3(seq1, seq2, seq3, y, gapcost, backtrack=False): 
     D = numpy.zeros(shape=(len(seq1)+1, len(seq2)+1, len(seq3)+1))
-    D12 = global_linear(seq1, seq2, y, gapcost)
-    D13 = global_linear(seq1, seq3, y, gapcost)
-    D23 = global_linear(seq2, seq3, y, gapcost)
+    D12 = global_linear(seq1, seq2, y, gapcost, matrix=True)
+    D13 = global_linear(seq1, seq3, y, gapcost, matrix=True)
+    D23 = global_linear(seq2, seq3, y, gapcost, matrix=True)
     for i in range(0, len(seq1)):
         for j in range(0, len(seq2)): 
             D[i, j, 0] = D12[i, j] + (i + j) * gapcost
@@ -134,7 +131,7 @@ def exact_3(seq1, seq2, seq3, y, gapcost, backtrack = False):
             j, k = j-1, k-1
         elif i > 0 and D[i, j, k] == D[i-1, j, k] + 2 * gapcost:
             upperAlign = upperAlign + a
-            middleAlign = middleAlign + '-' 
+            middleAlign = middleAlign + '-'
             lowerAlign = lowerAlign + '-'
             i = i-1
         elif j > 0 and D[i, j, k] == D[i, j-1, k] + 2 * gapcost:
@@ -144,7 +141,7 @@ def exact_3(seq1, seq2, seq3, y, gapcost, backtrack = False):
             j = j-1
         elif k > 0 and D[i, j, k] == D[i, j, k-1] + 2 * gapcost:
             upperAlign = upperAlign + '-'
-            middleAlign = middleAlign + '-' 
+            middleAlign = middleAlign + '-'
             lowerAlign = lowerAlign + c
             k = k-1
         else: 
@@ -155,3 +152,55 @@ def exact_3(seq1, seq2, seq3, y, gapcost, backtrack = False):
     print(lowerAlign[::-1])
 
     return D[len(seq1), len(seq2), len(seq3)]        
+
+
+def approx(y, gapcost, *seqs):
+    best_seq = None
+    best_score = -1
+
+    for seq1 in seqs: 
+        score = sum([global_linear(seq1, seq2, y, gapcost) for seq2 in seqs if seq2 != seq1])
+        if score > best_score:
+            best_score = score
+            best_seq = seq1
+
+    columns = [ [c] for c in best_seq ]
+    seqs = list(seqs)
+    seqs.remove(best_seq)
+    for seq in seqs: 
+        A = global_linear(best_seq, seq, y, gapcost, backtrack=True)
+        columns = extend(columns, *A)
+    return transpose_columns(columns)
+
+def transpose_columns(columns):
+    return [''.join([columns[i][j] for i in range(len(columns))]) for j in range(len(columns[0]))]
+
+def extend(columns, upperAlign, lowerAlign):
+    c, i = 0, 0
+    results = []
+    col_len = len(columns[0])
+    while(c < len(columns) or i < len(upperAlign)):
+        if c < len(columns) and columns[c][0] == '-':
+            columns[c].append('-')
+            results.append(columns[c])
+            c = c + 1
+            continue
+        if upperAlign[i] != '-' and lowerAlign[i] != '-':
+            columns[c].append(lowerAlign[i])
+            results.append(columns[c])
+            c, i = c + 1, i + 1
+            continue
+        if upperAlign[i] == '-':
+            temp = list('-' * col_len)
+            temp.append(lowerAlign[i])
+            results.append(temp)
+            i = i + 1
+            continue
+        if lowerAlign[i] == '-':
+            columns[c].append('-')
+            results.append(columns[c])
+            c, i = c + 1, i + 1
+            continue
+        print("SYSTEM FAILURE!!! PANIC")
+        sys.exit(1)
+    return results
---
+++ Proj2/sp_approx.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+import numpy
+import AiBS
+from Bio import SeqIO
+import argparse
+from itertools import permutations
+
+parser = argparse.ArgumentParser(description='Genome Sequence Analyzor')
+parser.add_argument('cost', type=str, help='Cost file to read')
+parser.add_argument('fasta', type=str, help='Fasta file to read')
+parser.add_argument('seqs', type=str, nargs='+', help='Sequences')
+
+args = parser.parse_args()
+
+fasta_sequences = SeqIO.to_dict(SeqIO.parse(open(args.fasta), 'fasta'))
+
+seqs = []
+for seq in args.seqs: 
+    seqs.append(str(fasta_sequences[seq].seq).lower())
+
+with open(args.cost, 'r') as cost_file:
+    letters = cost_file.readline().lower()
+    complex_ycost = numpy.matrix(cost_file.read())
+
+
+def input_y(a, b):
+    try:
+        return complex_ycost[letters.index(a), letters.index(b)]
+    except:
+        print('WTF ' + a + ' - ' + b + ' is not a real cost')
+        sys.exit(1)
+        return 0 
+
+alignment = AiBS.approx(input_y, 5, *seqs)
+for i in range(len(alignment)):
+    print(">seq" + str(i+1))
+    print(alignment[i])
