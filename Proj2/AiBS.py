cda8c5082f63279b9fe2b4b3a12e8ea4cad5eeed
Frederik Hansen <frederiklhansen@gmail.com>
2014-09-19 12:21:36 +0200

    numeru dos

--- Proj2/AiBS.py
+++ Proj2/AiBS.py
@@ -7,8 +7,10 @@ complex_ycost = numpy.matrix('0 5 2 5; 5 0 5 2; 2 5 0 5; 5 2 5 0')
 
 letters = 'ACGT'.lower()
 
+
 def complex_y(a, b):
     return complex_ycost[letters.index(a), letters.index(b)]
+
 
 undef = float("inf")
 
@@ -34,7 +36,7 @@ def global_linear(seq1, seq2, y, gapcost, bactrack = False):
             D[i, j] = min(v1, v2, v3)
 
     if not bactrack:
-        return D[len(seq1), len(seq2)]
+        return D
 
     upperAlign = ''
     lowerAlign = ''
@@ -61,4 +63,95 @@ def global_linear(seq1, seq2, y, gapcost, bactrack = False):
 
     return D[len(seq1), len(seq2)]
 
-def approx_3(seq1, seq2, seq3, y, gapcost)
+def exact_3(seq1, seq2, seq3, y, gapcost, backtrack = False): 
+    D = numpy.zeros(shape=(len(seq1)+1, len(seq2)+1, len(seq3)+1))
+    D12 = global_linear(seq1, seq2, y, gapcost)
+    D13 = global_linear(seq1, seq3, y, gapcost)
+    D23 = global_linear(seq2, seq3, y, gapcost)
+    for i in range(0, len(seq1)):
+        for j in range(0, len(seq2)): 
+            D[i, j, 0] = D12[i, j] + (i + j) * gapcost
+            D[i, 0, j] = D13[i, j] + (i + j) * gapcost
+            D[0, i, j] = D23[i, j] + (i + j) * gapcost
+
+    for i in range(1, len(seq1)+1):
+        for j in range(1, len(seq2)+1): 
+            for k in range(1, len(seq3)+1):
+                cij = y(seq1[i-1], seq2[j-1])
+                cik = y(seq1[i-1], seq3[k-1])
+                cjk = y(seq2[j-1], seq3[k-1])
+
+                d1 = D[i-1, j-1, k-1] + cij + cik + cjk
+                d2 = D[i-1, j-1, k] + cij + 2 * gapcost
+                d3 = D[i-1, j, k-1] + cik + 2 * gapcost
+                d4 = D[i, j-1, k-1] + cjk + 2 * gapcost
+                d5 = D[i-1, j, k] + 2 * gapcost
+                d6 = D[i, j-1, k] + 2 * gapcost
+                d7 = D[i, j, k-1] + 2 * gapcost
+
+                D[i, j, k] = min(d1, d2, d3, d4, d5, d6, d7)
+    if not backtrack: 
+        return D[len(seq1), len(seq2), len(seq3)]
+
+    upperAlign = ''
+    middleAlign = ''
+    lowerAlign = ''
+
+    i, j, k = len(seq1), len(seq2), len(seq3)
+    while i != 0 or j != 0 or k != 0:
+        a = seq1[i-1] if i > 0 else 0
+        b = seq2[j-1] if j > 0 else 0
+        c = seq3[k-1] if k > 0 else 0
+
+        cij = y(seq1[i-1], seq2[j-1]) if i > 0 and j > 0 else 0
+        cik = y(seq1[i-1], seq3[k-1]) if i > 0 and k > 0 else 0 
+        cjk = y(seq2[j-1], seq3[k-1]) if j > 0 and k > 0 else 0
+
+        d5 = D[i-1, j, k] + 2 * gapcost
+        d6 = D[i, j-1, k] + 2 * gapcost
+        d7 = D[i, j, k-1] + 2 * gapcost
+
+
+        if j>0 and i>0 and k>0 and D[i, j, k] == D[i-1, j-1, k-1] + cij + cik + cjk:
+            upperAlign = upperAlign + a
+            middleAlign = middleAlign + b
+            lowerAlign = lowerAlign + c
+            i, j, k = i-1, j-1, k-1
+        elif i > 0 and j > 0 and D[i, j, k] == D[i-1, j-1, k] + cij + 2 * gapcost:
+            upperAlign = upperAlign + a
+            middleAlign = middleAlign + b
+            lowerAlign = lowerAlign + '-'
+            i, j = i-1, j-1
+        elif i > 0 and k > 0 and D[i, j, k] == D[i-1, j, k-1] + cik + 2 * gapcost:
+            upperAlign = upperAlign + a
+            middleAlign = middleAlign + '-'
+            lowerAlign = lowerAlign + c
+            i, k = i-1, k-1
+        elif j > 0 and k > 0 and D[i, j, k] == D[i, j-1, k-1] + cjk + 2 * gapcost:
+            upperAlign = upperAlign + '-'
+            middleAlign = middleAlign + b
+            lowerAlign = lowerAlign + c
+            j, k = j-1, k-1
+        elif i > 0 and D[i, j, k] == D[i-1, j, k] + 2 * gapcost:
+            upperAlign = upperAlign + a
+            middleAlign = middleAlign + '-' 
+            lowerAlign = lowerAlign + '-'
+            i = i-1
+        elif j > 0 and D[i, j, k] == D[i, j-1, k] + 2 * gapcost:
+            upperAlign = upperAlign + '-'
+            middleAlign = middleAlign + b 
+            lowerAlign = lowerAlign + '-'
+            j = j-1
+        elif k > 0 and D[i, j, k] == D[i, j, k-1] + 2 * gapcost:
+            upperAlign = upperAlign + '-'
+            middleAlign = middleAlign + '-' 
+            lowerAlign = lowerAlign + c
+            k = k-1
+        else: 
+            print("LOL SYSTEM EXIT ONE!")
+            sys.exit(1)
+    print(upperAlign[::-1])
+    print(middleAlign[::-1])
+    print(lowerAlign[::-1])
+
+    return D[len(seq1), len(seq2), len(seq3)]        
--- Proj2/sp_exact_3.py
+++ Proj2/sp_exact_3.py
@@ -1,4 +1,4 @@
-! /usr/bin/python3
+#!/usr/bin/python3
 import sys
 import numpy
 import AiBS
@@ -12,14 +12,16 @@ parser.add_argument('fasta', type=str, help='Fasta file to read')
 parser.add_argument('seq1', type=str, help='Sequence 1')
 parser.add_argument('seq2', type=str, help='Sequence 2')
 parser.add_argument('seq3', type=str, help='Sequence 3')
+parser.add_argument('--backtrack', dest='backtrack', action='store_true')
+parser.set_defaults(backtrack=False)
 
 args = parser.parse_args()
 
 fasta_sequences = SeqIO.to_dict(SeqIO.parse(open(args.fasta),'fasta'))
 
-seq1 = str(fasta_sequences[args.seq1].seq)
-seq2 = str(fasta_sequences[args.seq2].seq)
-seq3 = str(fasta_sequences[args.seq2].seq)
+seq1 = str(fasta_sequences[args.seq1].seq).lower()
+seq2 = str(fasta_sequences[args.seq2].seq).lower()
+seq3 = str(fasta_sequences[args.seq3].seq).lower()
 
 with open(args.cost, 'r') as cost_file:
 	letters = cost_file.readline().lower()
@@ -35,3 +37,5 @@ def input_y(a, b):
 		print('WTF ' + a + ' - ' + b + ' is not a real cost')
 		sys.exit(1)
 		return 0 
+
+print(AiBS.exact_3(seq1, seq2, seq3, input_y, 5, args.backtrack))
